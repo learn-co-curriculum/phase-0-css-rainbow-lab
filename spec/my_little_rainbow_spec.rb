f230275ff5d870981418484526a6a1db098fe464
jenmyers <jen.myers@flatironschool.com>
2018-06-11 15:29:08 -0500

    Add CSS tests

--- spec/my_little_rainbow_spec.rb
+++ spec/my_little_rainbow_spec.rb
@@ -83,15 +83,4 @@ RSpec.describe 'Navigation and Nested Elements' do
 
   end
 
-  context 'w3c validation' do
-    it 'is a valid w3c document' do
-      validator = W3CValidators::NuValidator.new
-      html = File.read('./index.html')
-      results = validator.validate_text(html)
-
-      error_messages = "Expected a valid w3c document but got:\n#{results.errors.collect{|e| e.to_s}.join("\n")}"
-
-      expect(results.errors).to be_empty, error_messages
-    end
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,6 +97,6 @@ end
 
 def parsed_css
   parser = CssParser::Parser.new
-  parser.load_uri!('./style.css')
+  parser.load_uri!('main.css')
   parser
 end
